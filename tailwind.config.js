93560b54d0bbe833f6f3d35c22f239a268e2a14d
williamKhine <williamkhine@outlook.com>
2024-02-27 17:48:20 -0500

    facvions

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,39 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
+    theme: {
+        colors: {
+            'text': '#eefbda',
+            'background': '#0c1301',
+            'primary': '#bdf075',
+            'secondary': '#119776',
+            'accent': '#3ed3ea',
+        },
+        fontSize: {
+            sm: '0.750rem',
+            base: '1rem',
+            xl: '1.333rem',
+            '2xl': '1.777rem',
+            '3xl': '2.369rem',
+            '4xl': '3.158rem',
+            '5xl': '4.210rem',
+        },
+        fontFamily: {
+            heading: 'Ubuntu',
+            body: 'Ubuntu Mono',
+        },
+        fontWeight: {
+            normal: '400',
+            bold: '700',
+        },
+        container: {
+            center: true,
+            padding: '1rem',
+        },
     },
-  },
-  plugins: [],
+    plugins: [],
 };
