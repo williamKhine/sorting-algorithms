887dca2adddee6148712f7b217480b41b21b27ed
williamKhine <williamkhine@outlook.com>
2024-02-27 17:47:16 -0500

    basic layout and favicons

---
+++ src/app/_components/control-bar.js
@@ -0,0 +1,9 @@
+export default function ControlBar() {
+    return (
+        <div className={'bg-background opacity-50'}>
+            <div className={'container my-4'}>
+                <button>Sort</button>
+            </div>
+        </div>
+    );
+}
---
+++ src/app/_components/header.js
@@ -0,0 +1,14 @@
+import Link from 'next/link';
+
+export default function Header() {
+    return (
+        <nav className={'container my-4 flex items-center justify-between'}>
+            <Link href={'/'}>
+                <h1 className={'text-2xl md:text-3xl font-bold text-text text-opacity-0 bg-gradient-to-l from-primary to-secondary bg-clip-text'}>
+                    SORTING ALGORITHMS
+                </h1>
+            </Link>
+            <Link href={'/about'} className={'text-lg md:text-xl hover:text-primary hover:underline'}>About</Link>
+        </nav>
+    );
+}
